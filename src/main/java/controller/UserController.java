1c8ec1f582c5f27262964281e105b5d08c1bd7ef
liouyang25 <liouyang25@gmail.com>
2023-11-27 11:51:30 +0800

    UserController first commit

--- src/main/java/controller/UserController.java
+++ src/main/java/controller/UserController.java
@@ -1,6 +1,10 @@
 package controller;
 
 import model.User;
+import model.common.Msg;
+import service.UserService;
+import service.impl.UserServiceImpl;
+import utils.JSONUtil;
 
 /**
  * @author: 李卫
@@ -11,12 +15,33 @@ import model.User;
 public class UserController extends BaseController {
     private String username;
     private String password;
+    private UserService userService;
+
+    public UserController() {
+        userService = new UserServiceImpl();
+    }
 
     public String login() {
-        return null;
+        Msg msg = new Msg();
+        User user = new User();
+        user.setUsername(username);
+        user.setPassword(password);
+        user = userService.login(user);
+
+        if (user != null) {
+            context.put(LOGIN_USER, user);
+            msg.setType(Msg.SUCCESS);
+            msg.setMsg("用户登录成功");
+            log.info("用户登陆成功");
+        } else {
+            msg.setType(Msg.FAIL);
+            msg.setMsg("用户名或密码错误");
+            log.info("用户登录失败");
+        }
+        return JSONUtil.entity2JSON(msg);
     }
 
     public User getLoginUser() {
-        return null;
+        return context.get(LOGIN_USER) != null ? (User) context.get(LOGIN_USER) : null;
     }
 }
